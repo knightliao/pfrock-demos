6a47fdd1dc6c7f5cc541037cc0db7bbdf76fe5b1
knightliao <knightliao@gmail.com>
2016-03-04 16:52:35 +0800

    re

--- demos/http-demo/mocks/handler/hello_world.py
+++ demos/http-demo/mocks/handler/hello_world.py
@@ -5,8 +5,4 @@ from pfrock.core.web import PfrockRequestDispatcher
 
 class HelloWorldHandler(PfrockRequestDispatcher):
     def get(self):
-        self.write("Hello, world " + self.query + " " + str(self.page_no))
-
-    def initialize(self, **kwargs):
-        self.query = kwargs.get('query', "")
-        self.page_no = kwargs.get('pageno', 1)
+        self.write("Hello, world")
